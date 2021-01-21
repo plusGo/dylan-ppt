66e7f0275a308624be73e642ae04f2fd3a57144f
menghonglin <mhlplus@outlook.com>
2021-01-21 13:36:54 +0800

    doc：更新readme，未来需要做什么

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 - git hook
 
 
+# analysis
+## shape-box 用于选中一个或多个元素
+## selector-area 模拟鼠标实现选区
+## svg作为slide实现编辑
+## 渲染区与编辑区互相如何绑定
+
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import './style/defaule.theme.scss';
-import {WebPPTWorkspace} from './workspace/web-ppt-workspace';
-import {WebPPTPlayer} from './web/player/web-ppt-player';
+import {WebPPTWorkspace} from './lib/workspace/web-ppt-workspace';
+import {WebPPTPlayer} from './lib/web/player/web-ppt-player';
 
 const workspace = new WebPPTWorkspace('#ppt-workspace-container');
 let player = new WebPPTPlayer({workspace, data: null});
