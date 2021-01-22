ebe533c6cc8f009b45601c1b83163f0477eb763a
menghonglin <mhlplus@outlook.com>
2021-01-22 13:41:24 +0800

    feat:custom observable

---
+++ src/lib/obervable/observable.ts
@@ -0,0 +1,39 @@
+export interface Observer<T extends any, S extends any> {
+    next: (value?: T) => void;
+    error?: (error?: S) => void;
+    complete?: () => void;
+}
+
+export interface Subscription {
+    unsubscribe(): void;
+}
+
+
+export class Observable<T extends any, S extends any> {
+
+    constructor(private observerFunc: (observer: Observer<T, S>) => void) {
+    }
+
+    subscribe(nextFunc: (value?: T) => void,
+              errorFunc: (error?: S) => void = () => null,
+              completeFunc: () => void = () => null): Subscription {
+        this.observerFunc({
+            next: nextFunc,
+            error: errorFunc,
+            complete: completeFunc
+        })
+        return {
+            unsubscribe() {
+                this.resetObserverFunc()
+            }
+        }
+    }
+
+    resetObserverFunc(): void {
+        this.observerFunc({
+            next: () => null,
+            error: () => null,
+            complete: () => null
+        })
+    }
+}
