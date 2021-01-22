73e7f00baa4e0fee347e5a111d8bf683db4a8cfc
menghonglin <mhlplus@outlook.com>
2021-01-22 13:45:46 +0800

    fix:custom observable

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,15 @@ import './style/defaule.theme.scss';
 import {WebPPTWorkspace} from './lib/workspace/web-ppt-workspace';
 // import {WebPPTPlayer} from './lib/web/player/web-ppt-player';
 import {AreaSelector} from "./lib/editor/area-selector";
+import {Observable} from "./lib/obervable/observable";
 
 const workspace = new WebPPTWorkspace('#ppt-workspace-container');
 // let player = new WebPPTPlayer({workspace, data: null});
 // player.play();
 
 new AreaSelector(workspace.instanceElement);
+
+new Observable((observer => {
+    console.log('observable 1',observer)
+}))
 
